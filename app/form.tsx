68907687113040ca6c3a3fcc1cc0b7f4214f0d87
Hari <neogalaxy45@gmail.com>
2023-04-29 20:52:33 +0700

    fix error

--- app/form.tsx
+++ app/form.tsx
@@ -16,7 +16,7 @@ export default function Form() {
           Stay tuned for the latest updates, exclusive offers, and exciting news about our upcoming products.
          </p>
          <p className="text-white text-[20px] leading-[24px] font-normal mt-2 font-inter">
-          We can't wait to share them with you
+          We can&apos;t wait to share them with you
          </p>
         </>
         :
@@ -33,7 +33,7 @@ export default function Form() {
             className="bg-black border-white border rounded-md px-3 py-2 w-[342px]"
             onChange={(e:any) => setInput(e.target.value)}
           />
-          <button className="w-[108px] relative before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] bg-black border-white border rounded-md px-4 py-2 flex items-center justify-center gap-2.5 h-[42px]" onClick={() => setSuccess(true)}>
+          <button className="w-[108px] bg-black border-white border rounded-md px-4 py-2 flex items-center justify-center gap-2.5 h-[42px]" onClick={() => setSuccess(true)}>
             Submit
           </button>
         </div>
